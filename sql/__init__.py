7657de2cd3e2ebe205db8b3eeb07a0555d1424bc
Fabian Gärtner <fabian@bioinf.uni-leipzig.de>
2017-07-06 22:41:37 +0200

    Update SQLite functions

--- sql/__init__.py
+++ sql/__init__.py
@@ -1,4 +1,4 @@
-from sqlite3 import connect
+from sqlite3 import connect, OperationalError
 from orcid import OrcID
 
 class DB:
@@ -12,6 +12,23 @@ class DB:
 	
 	def close(self):
 		self.conn.close()
-	
+	def createDB(self):
+		self.c.execute("CREATE TABLE people (orcid CHARACTER(16), start DATE, end DATE)")
+		self.conn.commit()
+	def dropDB(self):
+		try:
+			self.c.execute("DROP TABLE people")
+			self.conn.commit()
+		except OperationalError:
+			pass
+	def addUser(self, orchid, start, stop):
+		self.c.execute("INSERT INTO people VALUES (?,?,?)", (orchid, start, stop))
+		self.conn.commit()
 	getOrcIds = lambda self: [OrcID(*t) for t in self.getList()]
+	def createTestDB(self):
+		self.dropDB()
+		self.createDB()
+		self.addUser("0000000219094153","1900-01-01","2016-12-31")
+		self.addUser("000000020183570X","1900-01-01","2016-12-31")
+		self.addUser("0000000303977442","1900-01-01","2016-12-31")
 	
